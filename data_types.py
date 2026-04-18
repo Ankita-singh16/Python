c243b080f976b5642021cfb01b5aad37ff92ee30
Ankita <ankitasingh12131213@gmail.com>
2026-04-18 11:15:05 +0530

    Add files via upload

---
+++ data_types.py
@@ -0,0 +1,43 @@
+# int - stores whole numbers in which +ve numbers, -ve numbers and also 0
+
+num = 120
+
+print(num)
+print(type(num))
+
+# float - stores decimal values
+
+PI = 3.14
+
+print(PI)
+print(type(PI))
+
+
+# String - String store text , strings are immutable in pyhton
+
+name = "Ankita"
+
+print(name)
+print(type(name))
+
+# boolean - Stores True or False
+
+isPrime = True
+
+print(isPrime)
+print(type(isPrime))
+
+# None - It represents nothing or no values
+
+value = None
+
+print(value)
+print(type(value))
+
+
+
+# tot_price -> Snake case
+# totPrice -> Camel case
+# TotPrice -> Pascal case
+
+
