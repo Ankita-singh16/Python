e246b4ff006e286842947396f7032cd7d4a2b1de
Ankita <ankitasingh12131213@gmail.com>
2026-04-19 22:28:47 +0530

    Add files via upload

---
+++ Variables.py
@@ -0,0 +1,11 @@
+name = "Ankita"
+age = 20
+marks = 76
+
+print(name)
+print("My name is :",name)
+print("My age is :" , age-1)
+
+# In python identation is very important
+# name, age, marks are identifiers
+# In python indentifiers are case sensitive language
