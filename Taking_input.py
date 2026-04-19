24c22f5edf2a4165bb1011feebdc7522ea91d552
Ankita <ankitasingh12131213@gmail.com>
2026-04-19 22:29:15 +0530

    Add files via upload

---
+++ Taking_input.py
@@ -0,0 +1,19 @@
+# username = input("Enter your name :")
+# print("Welcome ", username)
+
+# Sum of two number
+
+# a = int(input("Enter num1 : "))
+# b = int(input("Enter num2 : "))
+
+# sum = a + b
+# print(sum)
+
+# Avg of two number
+
+num1 = int(input("Enter num1 : "))
+num2 = int(input("Enter num2 : "))
+
+avg = (num1 + num2)/2
+
+print(avg)
