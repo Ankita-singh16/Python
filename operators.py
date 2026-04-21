93e66f8cd6c62c83f80fb2628857b931172b4d08
Ankita <ankitasingh12131213@gmail.com>
2026-04-21 18:21:34 +0530

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,42 @@
+# Arithmetic Operator - +,-,*,/,%,**
+
+a = 10
+b = 5
+ 
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a % b)
+print(a ** b)
+
+# Relational/Comparison Operators - >, >=, <, <=, ==, !=
+
+a = 20
+b = 10
+
+print(a < b)
+print(a > b)
+print(a <= b)
+print(a >= b)
+print(a == b)
+print(a != b)
+
+# Assignment Operator - =, +=, -=, *=, /=, **=
+
+a = 10
+b = 20
+
+a += 5
+print(a)
+b -= 10
+print(b)
+
+
+
+# Logical Opearator - and, or, not
+
+x = 5
+print(x > 2 and x < 10)
+print(x > 10 or x < 10)
+print(not(x > 2)) 
